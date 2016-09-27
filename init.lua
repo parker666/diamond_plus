50fb294f788280fc6f55ae07e24a8aca427eeb73
parker666 <parkerdmarshall@gmail.com>
2016-09-27 11:53:57 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,12 +2,9 @@ minetest.register_tool("diamond_plus:diamond_xpick", {
     description = "diamond_xpick",
     inventory_image = "blue_pick.png",
     tool_capabilities = {
-        max_drop_level=3,
+        max_drop_level=20,
         groupcaps= {
-            cracky={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
-            crumbly={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
-            choppy={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
-            snappy={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
+            cracky={times={[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
         }
     }
 })
