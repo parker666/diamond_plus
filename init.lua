15801505c43f3497ccbc9be58e7617436519ad57
parker666 <parkerdmarshall@gmail.com>
2016-09-27 11:50:16 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,10 +4,10 @@ minetest.register_tool("diamond_plus:diamond_xpick", {
     tool_capabilities = {
         max_drop_level=3,
         groupcaps= {
-            cracky={times={[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
-            crumbly={times={[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
-            choppy={times={[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
-            snappy={times={[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
+            cracky={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
+            crumbly={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
+            choppy={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
+            snappy={times=[1]=1.00, [2]=1.50, [3]=2.00}, uses=70, maxlevel=20}
         }
     }
 })
